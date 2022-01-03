2f6a9cecc780692a23aa041e0d617c02e807db22
kong-bai233 <75122884+kong-bai233@users.noreply.github.com>
2022-01-03 09:51:18 +0800

    Update 石头剪刀布.py

--- 石头剪刀布.py
+++ 石头剪刀布.py
@@ -1,15 +1,17 @@
-print('石头(1) 剪刀(2) 布(3)\n')
 while 1:
     import random
-    player=int(input("请输入："))
-    computer=random.randint(1,3)
-    if player>3 or player<1:
+    player=input("请输入：")
+    computer=['石头','剪刀','布']
+    i=random.randint(0,2)
+    if player=='退出' or player=='exit':
+        exit()
+    if player!='石头' and player!='剪刀' and player!='布':
         print('无效操作!!!\n')
     else:
-        print('玩家 %d ----- 对方 %d'%(player,computer))
-        if (player==1 and computer==2) or (player==2 and computer==3) or (player==3 and computer==1):
+        print('玩家 %s ----- 对方 %s'%(player,computer[i]))
+        if (player=='石头' and computer[i]=='剪刀') or (player=='剪刀' and computer[i]=='布') or (player=='布' and computer[i]=='石头'):
             print('玩家胜！\n\n')
-        elif player==computer:
+        elif player==computer[i]:
             print('平局！\n\n')
         else:
             print('对方胜！\n\n')
